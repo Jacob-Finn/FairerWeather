07d88b04a6df96cc16b3ea00661d08b247a6ad09
JacobFinn <Jacob.Finn102@gmail.com>
2018-10-24 14:28:23 -0500

    Added files

---
+++ FairerWeather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FairerWeather
+//
+//  Created by Jacob Finn on 10/24/18.
+//  Copyright © 2018 Jacob Finn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
