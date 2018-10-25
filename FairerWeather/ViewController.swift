61a3bf5c3210d2a86fd54c7b1fa4f9277bfeba16
JacobFinn <Jacob.Finn102@gmail.com>
2018-10-25 14:15:48 -0500

    Updated gitignore and added details to storyboard

--- FairerWeather/ViewController.swift
+++ FairerWeather/ViewController.swift
@@ -7,14 +7,40 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
+
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let apiKeys = APIKeys()
+        // Do any additional setup after loading the view, typically from a nib
+        let darkSkyUrl = "https://api.darksky.net/forecast/" + apiKeys.darkSkyKey
+        let latitude = "37.8267"
+        let longitude = "-122.4233"
+        let url = darkSkyUrl + "/" + latitude + "," + longitude
+        print(url)
+        
+        
+        let request = Alamofire.request(url)
+        request.responseJSON { (response) in
+            switch response.result {
+            case .success(let value):
+                let json = JSON(value)
+                print(json)
+                
+                
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+            
+        }
+        
+        
     }
-
-
+    
+    
 }
 
