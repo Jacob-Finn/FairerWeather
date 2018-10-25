bad3d8569d409b7c40e4ff481c73585706ec9af7
JacobFinn <Jacob.Finn102@gmail.com>
2018-10-25 12:40:03 -0500

    Setup basic view controllers and added basic api struct

---
+++ FairerWeather/APIKeys.swift
@@ -0,0 +1,14 @@
+//
+//  APIKeys.swift
+//  FairerWeather
+//
+//  Created by Jacob Finn on 10/25/18.
+//  Copyright © 2018 Jacob Finn. All rights reserved.
+//
+
+import Foundation
+
+struct APIKeys {
+    let darkSkyKey = ""
+    let googleKey = ""
+}
